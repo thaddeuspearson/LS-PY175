094ae856e365b3b675f7446c885cb8080049ff0c
Thaddeus <thaddeuspearson@gmail.com>
2025-05-31 02:01:27 +0200

    WIP: add book_viewer app, through 'Before Request' ex

---
+++ book_viewer_starter/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    with open("book_viewer/data/toc.txt", "r") as file:
+        toc = file.readlines()
+    return render_template("home.html", toc=toc)
+
+
+@app.route("/chapters/<chapter_num>")
+def chapter(chapter_num):
+    with open("book_viewer/data/toc.txt", "r") as file:
+        toc = file.readlines()
+
+    chapter_name = toc[int(chapter_num) - 1]
+    chapter_title = f"Chapter {chapter_num}: {chapter_name}"
+
+    with open(f"book_viewer/data/chp{chapter_num}.txt", "r") as file:
+        chapter = file.read()
+
+    return render_template("chapter.html", toc=toc,
+                           chapter_title=chapter_title, chapter=chapter)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
