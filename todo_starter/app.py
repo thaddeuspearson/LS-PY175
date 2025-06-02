d48b94b11e6131c2f5d6a31523d861df4a27dc02
Thaddeus <thaddeuspearson@gmail.com>
2025-06-03 00:27:51 +0200

    add new_list template, ud lists template + app to dynamically generate links

--- todo_starter/app.py
+++ todo_starter/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 
 app = Flask(__name__)
@@ -6,7 +6,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('lists.html')
+    return redirect(url_for("get_lists"))
+
+
+@app.route("/lists/new")
+def add_todo_list():
+    return render_template('new_list.html')
+
+
+@app.route("/lists")
+def get_lists():
+    # TO REMOVE: dummy data for dev
+    lists = [
+        {"title": "Lunch Groceries", "todos": []},
+        {"title": "Dinner Groceries", "todos": []},
+    ]
+    return render_template('lists.html', lists=lists)
 
 
 if __name__ == "__main__":
