501dbcb58dab3a00f0b25077b35e8065784e9229
Thaddeus <thaddeuspearson@gmail.com>
2025-06-02 23:22:33 +0200

    add layout, lists templates, ud app.py to render them

--- todo_starter/app.py
+++ todo_starter/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>This is a todo starter!</h1>"
+    return render_template('lists.html')
 
 
 if __name__ == "__main__":
