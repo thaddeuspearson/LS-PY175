bc9dd41a0acae2e5adc7b1e77efde5611f9913e4
Thaddeus <thaddeuspearson@gmail.com>
2025-06-08 23:08:34 +0200

    add index.html and cms dummy data

--- cms/app.py
+++ cms/app.py
@@ -1,13 +1,23 @@
 from flask import (
     Flask,
+    render_template,
 )
+import os
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Getting Started"
+    root = os.path.abspath(os.path.dirname(__file__))
+    data_dir = os.path.join(root, "cms", "data")
+    filenames = [
+        f for f in os.listdir(data_dir)
+        if os.path.isfile(os.path.join(data_dir, f))
+    ]
+    print(os.path.abspath(os.path.dirname(__file__)))
+    return render_template('index.html', filenames=filenames)
 
 
 if __name__ == "__main__":
