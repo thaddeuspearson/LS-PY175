c8d296284f9150407df7be2e58ad039d0ceeafda
Thaddeus <thaddeuspearson@gmail.com>
2025-06-10 01:12:09 +0200

    add test cases for editting files feature

--- cms/tests/test_app.py
+++ cms/tests/test_app.py
@@ -37,3 +37,24 @@ class TestApp(unittest.TestCase):
         with self.client.get("about.md") as response:
             self.assertEqual(response.status_code, 200)
             self.assertIn("About", response.get_data(as_text=True))
+
+    def test_get_edit_file_content(self):
+        with self.client.get("/changes.txt/edit") as response:
+            data = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("Edit content", data)
+            self.assertIn('<button type="submit"', data)
+
+    def test_save_file(self):
+        with self.client.post(
+                "/changes.txt", data={'content': 'test content'}) as response:
+            self.assertEqual(response.status_code, 302)
+
+        with self.client.get(response.headers["Location"]) as response:
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("changes.txt has been updated.",
+                          response.get_data(as_text=True))
+
+        with self.client.get("/changes.txt") as response:
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("test content", response.get_data(as_text=True))
