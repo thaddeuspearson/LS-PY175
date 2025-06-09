23216a79f9b90e1fece37e2a5a4fe85a0fd575d9
Thaddeus <thaddeuspearson@gmail.com>
2025-06-09 23:49:07 +0200

    add test case for non existent file redirect

--- cms/tests/test_app.py
+++ cms/tests/test_app.py
@@ -12,7 +12,7 @@ class TestApp(unittest.TestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertIn("changes.txt", response.get_data(as_text=True))
-        self.assertIn("about.txt", response.get_data(as_text=True))
+        self.assertIn("about.md", response.get_data(as_text=True))
         self.assertIn("history.txt", response.get_data(as_text=True))
 
     def test_render_file_content(self):
@@ -20,3 +20,15 @@ class TestApp(unittest.TestCase):
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.content_type, "text/plain; charset=utf-8")
             self.assertIn("Guido van Rossum", response.get_data(as_text=True))
+
+    def test_display_file_content_redirects_not_found(self):
+        with self.client.get("/does_not_exist.txt") as response:
+            self.assertEqual(response.status_code, 302)
+
+        with self.client.get(response.headers["Location"]) as response:
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("does not exist", response.get_data(as_text=True))
+
+        with self.client.get("/") as response:
+            self.assertEqual(response.status_code, 200)
+            self.assertNotIn("does not exist", response.get_data(as_text=True))
