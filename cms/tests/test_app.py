bbc2bb1fd1ffac9a3f4841db54f78f080e33c30b
Thaddeus <thaddeuspearson@gmail.com>
2025-06-11 23:34:03 +0200

    add testcases for create_document

--- cms/tests/test_app.py
+++ cms/tests/test_app.py
@@ -12,7 +12,7 @@ class TestApp(unittest.TestCase):
         self.data_path = os.path.join(os.path.dirname(__file__), "data")
         os.makedirs(self.data_path, exist_ok=True)
 
-    def teardown(self):
+    def tearDown(self):
         shutil.rmtree(self.data_path, ignore_errors=True)
 
     def create_test_document(self, name, content=""):
@@ -33,28 +33,32 @@ class TestApp(unittest.TestCase):
         self.create_test_document("history.txt", "Billy Bob Joe Steve")
 
         with self.client.get("/history.txt") as response:
+            data = response.get_data(as_text=True)
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.content_type, "text/plain; charset=utf-8")
-            self.assertIn("Billy Bob Joe Steve", response.get_data(as_text=True))
+            self.assertIn("Billy Bob Joe Steve", data)
 
     def test_display_file_content_redirects_not_found(self):
         with self.client.get("/does_not_exist.txt") as response:
             self.assertEqual(response.status_code, 302)
 
         with self.client.get(response.headers["Location"]) as response:
+            data = response.get_data(as_text=True)
             self.assertEqual(response.status_code, 200)
-            self.assertIn("does not exist", response.get_data(as_text=True))
+            self.assertIn("does not exist", data)
 
         with self.client.get("/") as response:
+            data = response.get_data(as_text=True)
             self.assertEqual(response.status_code, 200)
-            self.assertNotIn("does not exist", response.get_data(as_text=True))
+            self.assertNotIn("does not exist", data)
 
     def test_display_file_content_md_file(self):
         self.create_test_document("about.md", "# About")
 
         with self.client.get("about.md") as response:
+            data = response.get_data(as_text=True)
             self.assertEqual(response.status_code, 200)
-            self.assertIn("<h1>About</h1>", response.get_data(as_text=True))
+            self.assertIn("<h1>About</h1>", data)
 
     def test_get_edit_file_content(self):
         self.create_test_document("changes.txt",
@@ -69,17 +73,48 @@ class TestApp(unittest.TestCase):
     def test_save_file(self):
         self.create_test_document("changes.txt",
                                   'This should be overwritten')
-        with self.client.post(
-                "/changes.txt", data={'content': 'test content'}) as response:
+        with self.client.post("/changes.txt",
+                              data={'content': 'test content'}) as response:
             self.assertEqual(response.status_code, 302)
 
         with self.client.get(response.headers["Location"]) as response:
+            data = response.get_data(as_text=True)
             self.assertEqual(response.status_code, 200)
-            self.assertIn("changes.txt has been updated.",
-                          response.get_data(as_text=True))
+            self.assertIn("changes.txt has been updated.", data)
 
         with self.client.get("/changes.txt") as response:
+            data = response.get_data(as_text=True)
             self.assertEqual(response.status_code, 200)
-            self.assertNotIn("this should be overwritten", 
-                             response.get_data(as_text=True))
-            self.assertIn("test content", response.get_data(as_text=True))
+            self.assertNotIn("this should be overwritten", data)
+            self.assertIn("test content", data)
+
+    def test_new(self):
+        with self.client.get("/new") as response:
+            data = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("<input", data)
+            self.assertIn("Create</button>", data)
+
+    def test_create_document(self):
+        with self.client.post("/new",
+                              data={"filename": "test_file.txt"}) as response:
+            self.assertEqual(response.status_code, 302)
+
+        with self.client.get(response.headers["Location"]) as response:
+            data = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("test_file.txt", data)
+            self.assertIn("test_file.txt was created.", data)
+
+        with self.client.post(
+                "/new", data={"filename": "test_file.txt"}) as response:
+            data = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 422)
+            self.assertIn("test_file.txt already exists.", data)
+
+    def test_create_document_without_filename(self):
+        with self.client.post(
+                "/new", data={"filename": ""}) as response:
+            data = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 422)
+            self.assertIn("A name is required.", data)
